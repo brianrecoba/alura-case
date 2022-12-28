2cb1627fef9908e3a85690e28c33f2862334df9a
brianrecoba <brian12fernandes@gmail.com>
2022-12-27 23:54:32 -0300

    first commit

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link'
+
+export default function 404() {
+  return(
+    <>
+    <h1>Ops.. Parece que você se perdeu</h1>
+    <Link href="/">
+      Retornar a página inicial 
+    </Link>
+    </>
+  )
+}
---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link';
+
+export default function FAQPage() {
+  return(
+    <div>
+      <h1>Alura Cases - Home</h1>
+      <Link href="/" >
+        Ir para home
+      </Link>
+    </div>
+  )
+}
